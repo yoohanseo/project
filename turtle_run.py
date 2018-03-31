547ae9326930f0ef4e443655ef72eb5546e1464f
LAPTOP-CTSC1G67\yoo <yuhanseo15@gmail.com>
2018-03-31 21:51:02 +0900

    중간단계

--- turtle_run.py
+++ turtle_run.py
@@ -3,24 +3,47 @@
 #먹이 먹고나면 또 랜덤으로 생성
 #잡히면 죽음
 import turtle as t
+import random as r
+
+Devil=t.Turtle()
+Devil.shape('turtle')
+Devil.color('black')
+Devil.speed(0)
+Devil.up()
+Devil.goto(r.randint(-299,229),r.randint(-299,299))
+
+prey=t.Turtle()
+prey.shape('circle')
+prey.color('violet')
+prey.up()
+prey.goto(r.randint(-299,229),r.randint(-299,299))
+
+
+def turn_up ():
+	t.setheading(90)
+def turn_down ():
+	t.setheading(270)
+def turn_right ():
+	t.setheading(0)
+def turn_left ():
+	t.setheading(180)
+def play ():
+	t.fd(10)
+	ang=t.towards(t.pos())
+	Devil.setheading(ang)
+	Devil.fd(8)
+	
+
 
 t.shape('turtle')
 t.setup(500,500)
 t.bgcolor('gray')
-t.speed(0)
+t.speed(3)
 t.color('white')
 t.up()
-t.onkeypress(up,'Up')
-t.onkeypress(down,'Down')
-t.onkeypress(right,'Right')
-t.onkeypress(left,'Left')
-
-def up ():
-
-def down ():
-
-def right ():
-
-def left ():
-	
+t.onkeypress(turn_up,'Up')
+t.onkeypress(turn_down,'Down')
+t.onkeypress(turn_right,'Right')
+t.onkeypress(turn_left,'Left')
+t.listen()
 t.mainloop()
