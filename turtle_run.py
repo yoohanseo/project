b7bb79cebef68fe6ff5b808dd6e818e523ac0e4e
LAPTOP-CTSC1G67\yoo <yuhanseo15@gmail.com>
2018-03-31 21:08:10 +0900

    화면구성 단계

---
+++ turtle_run.py
@@ -0,0 +1,26 @@
+#화면구성
+#위,아래,옆 이동하는 키
+#먹이 먹고나면 또 랜덤으로 생성
+#잡히면 죽음
+import turtle as t
+
+t.shape('turtle')
+t.setup(500,500)
+t.bgcolor('gray')
+t.speed(0)
+t.color('white')
+t.up()
+t.onkeypress(up,'Up')
+t.onkeypress(down,'Down')
+t.onkeypress(right,'Right')
+t.onkeypress(left,'Left')
+
+def up ():
+
+def down ():
+
+def right ():
+
+def left ():
+	
+t.mainloop()
